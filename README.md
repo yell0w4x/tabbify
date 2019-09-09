981f73993b754bb119df2eefaf2ce725777a1d2c
dt <asdf@asdf.asdf>
2019-09-09 19:49:54 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# tabbify
-HTML tab control
+# tabbify jQuery plugin
+
+`ul` element specifies tabs and `div` with `panes` class defines panels. You can either use `id` and `href` 
+attributes to link up tabs with panes together or just leave it blank. In latter zero based index is used to make things work.
+Use predefined css or define your own rules to make desired appearance.
+
+```html
+<link rel="stylesheet" href="/tabbify/dist/tabbify.min.css">
+```
+```html
+<script src="/tabbify/dist/tabbify.min.js"></script>
+```
+
+```html
+<div id="tabs" class="tabs">
+    <ul>
+        <li><a href="#" class="active">Foreword</a></li>
+        <li><a href="#">Main</a></li>
+        <li><a href="#afterword">Afterword</a></li>
+    </ul>
+
+    <div class="panes">
+        <div class="tab-pane active">
+            <h1>Foreword</h1>
+        </div>
+
+        <div class="tab-pane">
+            <h1>Main</h1>
+        </div>
+        
+        <div class="tab-pane" id="#afterword">
+            <h1>Afterword</h1>
+        </div>
+    </div>
+</div>
+
+<script>
+$(function() {
+    var tabs = $('#tabs').tabbify({onChange: function(index) { alert(index); } });
+    tabs.next();
+});
+</script>
+```
+
+## Options
+
+* onChange(activeTabIndex, tabs, panes)
+
+this - jQuery wrapper on the element passed to tabbify, the same object returned by tabbify() and API accessible through.
+activeTabIndex - Index of the currently active tab beginnig from zero after change occurs.
+tabs - 'ul > li > a' jQuery tabs collection.
+panes - '.tab-pane' jQuery panes collection.
+
+## API
+
+* prev() - Select previous tab
+* next() - Select next tab
+* changeTab(index) - Select tab having index specified.
+* tabs() - 'ul > li > a' jQuery tabs collection.
+* panes() - '.tab-pane' jQuery panes collection.
+* isFirst() - whether first tab selected.
+* isLast() - whether last tab selected.
+
+First three also return tabbify api object so calls can be chained like `tabs.next().next()`.
---
+++ src/tabbify.js
@@ -0,0 +1,89 @@
+// tabbify 0.0.1
+
+(function tabControlPlugin($) {
+    $.fn.tabbify = function(options) {
+        var options = options || {};
+
+        if (this.length > 1) {
+            this.each(function() { $(this).tabbify(options); });
+            return this;
+        }
+
+        var activeTabIndex = 0;
+        var activeTabName = '';
+        var activePane = null;
+
+        var tabs = $(this).find('ul li > a');
+        var panes = $(this).find('.tab-pane');
+        var thiz = this;
+
+        tabs.click(function(e) {
+            tabs.removeClass('active');
+            activeTabName = $(e.target).attr('href');
+
+            tabs.each(function(i, item) {
+                var tabName = $(item).attr('href');
+
+                if (e.target == item) {
+                    activeTabIndex = i;
+                    $(e.target).addClass('active');
+                } else {
+                    var pane = $(tabName);
+                    if (pane.length == 0)
+                    {
+                        pane = $(panes[i]);
+                    }
+
+                    pane.removeClass("active");
+                    pane.css("display", "none");
+                }
+            });
+
+            activePane = $(activeTabName);
+            if (activePane.length == 0) { activePane = $(panes[activeTabIndex]); }
+            activePane.fadeIn();
+
+            callOnChange.call(thiz);
+
+            return false;
+        });
+
+        function callOnChange() {
+            if (options.onChange) {
+                options.onChange.call(this, activeTabIndex, tabs, panes);
+            }
+        }
+
+        this.prev = function() {
+            if (activeTabIndex > 0) {
+                tabs[--activeTabIndex].click();
+            }
+
+            return thiz;
+        };
+
+        this.next = function() {
+            if (activeTabIndex < tabs.length) {
+                tabs[++activeTabIndex].click();
+            }
+
+            return thiz;
+        };
+
+        this.changeTab = function(index) {
+            if (index < tabs.length && index >= 0) {
+                tabs[index].click();
+            }
+
+            return thiz;
+        };
+
+        this.selectedTabIndex = function() { return activeTabIndex; };
+        this.tabs = function() { return tabs; };
+        this.panes = function() { return panes; };
+        this.isFirst = function() { return activeTabIndex == 0; };
+        this.isLast = function() { return activeTabIndex == tabs.length - 1; };
+
+        return this;
+    }
+})(jQuery);
